2e0672b35440efdc78039aabf8001cc542aa58b3
Gilbert Icheboi <gilbert.icheboi@student.moringaschool>
2025-03-06 21:48:29 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {};
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {};
+}
+
